2659917b51e0f3b98b2fec155d438e32142999c9
Artem Egorof <areg@goldcarrot.ru>
2023-06-01 17:27:58 +0300

    build: new project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,124 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<HeaderNav />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from "./components/HeaderNav.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		HeaderNav
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Manrope", sans-serif;
+}
+
+* {
+	padding: 0px;
+	margin: 0px;
+	border: none;
+}
+
+*,
+*::before,
+*::after {
+	box-sizing: border-box;
+}
+
+a,
+a:link,
+a:visited {
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: none;
+}
+
+aside,
+nav,
+footer,
+header,
+section,
+main {
+	display: block;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+	font-size: inherit;
+	font-weight: inherit;
+}
+
+ul,
+ul li {
+	list-style: none;
+}
+
+img {
+	vertical-align: top;
+}
+
+img,
+svg {
+	max-width: 100%;
+	height: auto;
+}
+
+address {
+	font-style: normal;
+}
+
+input,
+textarea,
+button,
+select {
+	font-family: inherit;
+	font-size: inherit;
+	color: inherit;
+	background-color: transparent;
+}
+
+input::-ms-clear {
+	display: none;
+}
+
+button,
+input[type="submit"] {
+	display: inline-block;
+	box-shadow: none;
+	background-color: transparent;
+	background: none;
+	cursor: pointer;
+}
+
+input:focus,
+input:active,
+button:focus,
+button:active {
+	outline: none;
+}
+
+button::-moz-focus-inner {
+	padding: 0;
+	border: 0;
+}
+
+label {
+	cursor: pointer;
+}
+
+legend {
+	display: block;
 }
 </style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,126 @@
+<template>
+	<header class="header">
+		<div class="header__container">
+			<div class="header__logotip">
+				<img class="header__logotip-images" src="@/assets/svg/logotip.svg" alt="Логотип" />
+			</div>
+
+			<div class="header__right">
+				<div class="header__right-wrapper">
+					<div class="header__buttons">
+						<button class="header__button">Login</button>
+					</div>
+					<div class="header__languages">
+						<select class="header__language-select">
+							<option class="header__language-option" value="en">
+								<img class="header__language-images" src="@/assets/svg/logotip.svg" alt="English" />
+								EN
+							</option>
+							<option class="header__language-option" value="ru">
+								<img class="header__language-image" src="" alt="Russian" />
+								RU
+							</option>
+						</select>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "HeaderNav"
+};
+</script>
+
+<style lang="scss">
+.header {
+	background: linear-gradient(225deg, rgba(255, 255, 255, 0.46) 0.78%, rgba(255, 255, 255, 0) 100%) #6788f3;
+	height: 82px;
+
+	&__container {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		height: 100%;
+		margin: 0 auto;
+		max-width: 1200px;
+		padding: 0 20px;
+	}
+
+	&__logotip-images {
+		cursor: pointer;
+	}
+
+	&__button {
+		padding: 10px 32px 10px 32px;
+		color: #fff;
+		border: none;
+		cursor: pointer;
+		font-size: 16px;
+	}
+
+	&__buttons {
+		width: 106px;
+		height: 44px;
+		background-color: #6788f3;
+		border-radius: 8px;
+	}
+
+	&__languages {
+		display: flex;
+		align-items: center;
+	}
+
+	&__logotip img {
+		width: 275px;
+		height: 32px;
+	}
+
+	&__right-wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0 36px;
+	}
+
+	&__languages {
+		position: relative;
+		background: #e3e4e8;
+		width: 64px;
+		height: 32px;
+		border-radius: 8px;
+	}
+
+	&__language-select {
+		display: flex;
+		align-items: center;
+		width: 60px;
+		height: 28px;
+		appearance: none;
+		background: #ffffff;
+		box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.01), 0px 3px 1px rgba(0, 0, 0, 0.03);
+		border-radius: 6px;
+		border: none;
+		text-align: end;
+		margin: 2px;
+		outline: none;
+		padding: 5px;
+		cursor: pointer;
+		font-size: 14px;
+		font-weight: bold;
+		color: #7b8193;
+	}
+
+	&__language-option {
+		padding: 5px;
+		cursor: pointer;
+	}
+
+	&__language-images {
+		width: 16px;
+		height: 16px;
+	}
+}
+</style>
